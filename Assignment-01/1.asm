2ffd771e6602634ecdfcb1049feffef6fb177e90
AMARJEET SAINI <amarjeets167@gmail.com>
2021-09-20 22:35:48 +0530

    Add files via upload

--- Assignment-01/1.asm
+++ Assignment-01/1.asm
@@ -1,1 +1,12 @@
+.text 
+.globl main
 
+main: li $v0, 4         #system call for string
+      la $a0, myname    #load  base address of string
+      syscall
+
+      li $v0,10         #system call for exit
+      syscall
+
+.data 
+myname: .asciiz "Hello World!"
